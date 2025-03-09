d6143ae6128893a084cc4c66f32af1fd2e2429d2
Ava Tran <ava.tran1011@gmail.com>
2025-03-08 23:49:24 -0500

    Task 2: employee cards dynamic addition

---
+++ cc_13.js
@@ -0,0 +1,28 @@
+//task 2: adding employee cards dynamically
+function createElement(name, position) //function that creates the employee card
+{
+    const card = document.createElement("div");
+    card.setAttribute("class", "employee-card"); //setting the attributes
+
+    //each employee card will include the employee's name, position, and a remove button: 
+    const heading = document.createElement("h3"); //heading for employee's name on the card
+    heading.textContext = name;
+    const paragraph = document.createElement("p"); //paragraph of employee's position
+    paragraph.textContent = position;
+    const removeButton = document.createElement("button"); //remove button to delte the card
+    removeButton.textContent = "Remove";
+    removeButton.setAttribute("class", "remove-btn");
+    //when the button is clicked the card will actually be removed
+    removeButton.addEventListener('click', () => {card.remove();}); 
+
+    //appending the employee cards to the "employeeComtainer"
+    card.appendChild(heading); //adding employee's name
+    card.appendChild(paragraph); //adding employee's position
+    card.appendChild(removeButton); //adding a remove button
+    const employeeComtainer = document.getElementById("employeeContainer");
+    employeeComtainer.appendChild(card);
+}
+//employee's names and positions 
+createElement("Jennifer Jareau", "Communications Liason of the BAU");
+createElement("Spencer Reid", "SSA of the BAU");
+createElement("Penelope Garcia", "Technical Analyst of the BAU");
